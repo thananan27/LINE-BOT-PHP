2f6f86eeee250f9372d10b9515fb9c8419b44b00
thananan27 <thananan27@gmail.com>
2018-02-17 13:20:29 +0700

    Update line.php

--- line.php
+++ line.php
@@ -14,7 +14,7 @@ function send_LINE($msg){
       $url = 'https://api.line.me/v2/bot/message/push';
       $data = [
 
-        'to' => 'Ue77a191627f6ac91899e75d92264310c',
+        'to' => 'Ucf28a74d2ae4326589ec03a323bfbff0',
         'messages' => [$messages],
       ];
       $post = json_encode($data);
