411dc227167b487718f7440568a0257d89b28a4f
thananan27 <thananan27@gmail.com>
2018-03-09 16:48:01 +0700

    Update line.php

--- line.php
+++ line.php
@@ -4,14 +4,24 @@
 function send_LINE($msg){
  $access_token = 'bvU+0BZagThDznc/cOZb5bl2+hbQ0cgNcDxq7faxZymmwr3rvtOYf5L9Yy7ZPqD7CqdtfI0bCRif+T7Kx2qLiaVWOm8Dte7Rt2HhbHg2SJbNtMqd91LGRqkv3a7BIt/QHeKnAOaayZKeYZH5uGMJ3wdB04t89/1O/w1cDnyilFU='; 
 
-  $messages = [
-        'type' => 'text',
-        'text' => $msg
-        //'text' => $text
-      ];
-
-      // Make a POST Request to Messaging API to reply to sender
-      $url = 'https://api.line.me/v2/bot/message/reply';
+ // Get POST body content
+$content = file_get_contents('php://input');
+// Parse JSON
+$events = json_decode($content, true);
+// Validate parsed JSON data
+if (!is_null($events['events'])) {	
+// Loop through each event	
+foreach ($events['events'] as $event) {		
+// Reply only when message sent is in 'text' format		
+if ($event['type'] == 'message' && $event['message']['type'] == 'text') {			
+// Get text sent  
+$text = $event['message']['text'];			
+// Get replyToken 
+$replyToken = $event['replyToken'];			
+// Build message to reply back	
+$messages = ['type' => 'text',	'text' => $text	];			
+// Make a POST Request to Messaging API to reply to sender	
+$url = 'https://api.line.me/v2/bot/message/reply';
 $data = ['replyToken' => $replyToken,'messages' => [$messages],];
 $post = json_encode($data);
 $headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
@@ -27,3 +37,4 @@ echo $result . "";
   }	
  }}
  echo "OK";
+ ?>
