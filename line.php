ff1254c503ef12d1fe5544edf84cb67b2c15e824
thananan27 <thananan27@gmail.com>
2018-03-13 14:08:58 +0700

    Update line.php

--- line.php
+++ line.php
@@ -9,12 +9,11 @@ function send_LINE($msg){
         'text' => $msg
         //'text' => $text
       ];
-  $events = json_decode($content, true);	
-// Get replyToken 
-  $replyToken = $event['replyToken'];
-      // Make a POST Request to Messaging API to reply to sender
-      $url = 'https://api.line.me/v2/bot/message/reply';
-      $data = ['replyToken' => $replyToken,'messages' => [$messages],];
+      $url = 'https://api.line.me/v2/bot/message/push';
+      $data = [
+        'to' => 'Ucf28a74d2ae4326589ec03a323bfbff0',
+        'messages' => [$messages],
+      ];
       $post = json_encode($data);
       $headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
 
